2c2c15b12c273c9c6a5b007805c26242dc50e6ba
sejum-x <franchukivan123@gmail.com>
2023-10-17 17:26:34 +0300

    class array

--- lib/backend/array.dart
+++ lib/backend/array.dart
@@ -68,4 +68,64 @@ class SortingHelper {
     stopwatch.stop();
     return stopwatch.elapsedMilliseconds / 1000;
   }
+
+  static List<int> mergeSort(List<int> array) {
+    if (array.length <= 1) {
+      return array;
+    }
+
+    final int middle = array.length ~/ 2;
+    final List<int> left = array.sublist(0, middle);
+    final List<int> right = array.sublist(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+  static List<int> merge(List<int> left, List<int> right) {
+    List<int> result = [];
+    int i = 0, j = 0;
+
+    while (i < left.length && j < right.length) {
+      if (left[i] < right[j]) {
+        result.add(left[i]);
+        i++;
+      } else {
+        result.add(right[j]);
+        j++;
+      }
+    }
+
+    result.addAll(left.sublist(i));
+    result.addAll(right.sublist(j));
+
+    return result;
+  }
+
+  static List<int> countingSort(List<int> array) {
+    int max = array.reduce((value, element) => value > element ? value : element);
+    int min = array.reduce((value, element) => value < element ? value : element);
+    int range = max - min + 1;
+
+    List<int> count = List.filled(range, 0);
+    List<int> output = List.filled(array.length, 0);
+
+    for (int i = 0; i < array.length; i++) {
+      count[array[i] - min]++;
+    }
+
+    for (int i = 1; i < range; i++) {
+      count[i] += count[i - 1];
+    }
+
+    for (int i = array.length - 1; i >= 0; i--) {
+      output[count[array[i] - min] - 1] = array[i];
+      count[array[i] - min]--;
+    }
+
+    for (int i = 0; i < array.length; i++) {
+      array[i] = output[i];
+    }
+
+    return array;
+  }
 }
